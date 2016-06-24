f7e0ed81c0af531b090c6ead341d1f191076c653
Abish-R <abish.r01@gmail.com>
2016-06-24 18:29:39 +0530

    qq

---
+++ src/main/java/com/ftv_fashionshop/helixtech_android/worktestingcode/ConstantCricket.java
@@ -0,0 +1,11 @@
+package com.ftv_fashionshop.helixtech_android.worktestingcode;
+
+/**
+ * Created by helixtech-android on 24/6/16.
+ */
+public class ConstantCricket {
+
+    String maintitle[]={"History","Rules","Records"};
+    String subtitle[]={"","","","","",""};
+    String text[]={"","","","","",""};
+}
